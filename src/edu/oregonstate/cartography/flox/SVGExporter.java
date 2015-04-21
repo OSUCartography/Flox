81ea9078bfd6b8cadd5d189b43cd4e43414124fd
Bernie Jenny <jennyb@geo.oregonstate.edu>
2015-04-21 15:15:01 -0700

    Added import of WKT and export to SVG

---
+++ src/edu/oregonstate/cartography/flox/SVGExporter.java
@@ -0,0 +1,331 @@
+package edu.oregonstate.cartography.flox;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Point;
+import ika.utils.ColorUtils;
+import java.awt.Color;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.text.DecimalFormat;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Exporter for SVG format.
+ *
+ * @author Bernhard Jenny, Cartography and Geovisualization Group, Oregon State
+ * University
+ */
+public class SVGExporter {
+
+    /**
+     * rounding of coordinates
+     */
+    private final DecimalFormat df = new DecimalFormat("#.##");
+
+    private static final String SVGNAMESPACE = "http://www.w3.org/2000/svg";
+    private static final String XLINKNAMESPACE = "http://www.w3.org/1999/xlink";
+    private static final String XMLEVENTSNAMESPACE = "http://www.w3.org/2001/xml-events";
+
+    /**
+     * Factor to convert from millimeter to pixels. Assumes 72 pixels per inch.
+     */
+    private static final double MM2PX = 72. / 2.54 / 10.;
+
+    /**
+     * Name of the author creating the SVG document
+     */
+    private final String authorName;
+
+    /**
+     * Name of the application creating the SVG document
+     */
+    private final String applicationName;
+
+    /**
+     * Geometry features to export.
+     */
+    private final GeometryCollection collection;
+
+    /**
+     * Bounding box of the geometry collection that is exported.
+     */
+    private final Envelope collectionBoundingBox;
+
+    /**
+     * Denominator of the representative fraction of the map, e.g. 25000 for a
+     * scale of 1:25,000
+     */
+    private double mapScale = 1000;
+
+    /**
+     * Creates a new SVGExporter.
+     *
+     * @param collection The geometries to export.
+     * @param authorName Name of the author creating the SVG document.
+     * @param applicationName Name of the application creating the SVG document.
+     */
+    public SVGExporter(GeometryCollection collection, String authorName, String applicationName) {
+        this.authorName = authorName;
+        this.applicationName = applicationName;
+        this.collection = collection;
+        collectionBoundingBox = collection.getEnvelopeInternal();
+    }
+
+    /**
+     * Exports a GeometryCollection to a SVG document.
+     *
+     * @param outputStream The OutputStream that will be used to export to.
+     * @throws java.io.IOException Throws an exception if export is not
+     * possible.
+     */
+    public void export(OutputStream outputStream)
+            throws IOException {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            // construct the SVG root element
+            Element svgRootElement = createSVGRootElement(document);
+            document.appendChild(svgRootElement);
+
+            // add a description element
+            appendDescription(svgRootElement, document);
+
+            // convert GeoSet to SVG DOM
+            appendGeometryCollection(this.collection, svgRootElement, document);
+
+            // Prepare the output file
+            OutputStreamWriter outputStreamWriter
+                    = new OutputStreamWriter(outputStream, "utf-8");
+            StreamResult result = new StreamResult(outputStreamWriter);
+
+            // Write the DOM document to the file
+            /*
+             There is a bug in Java 1.5: XML output is not indented.
+             To work around this bug:
+             http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6296446
+             (1)set the indent-number in the transformerfactory
+             TransformerFactory tf = new TransformerFactory.newInstance();
+             tf.setAttribute("indent-number", new Integer(2));
+             (2)enable the indent in the transformer
+             Transformer t = tf.newTransformer();
+             t.setOutputProperty(OutputKeys.INDENT, "yes");
+             (3)wrap the otuputstream with a writer (or bufferedwriter)
+             t.transform(new DOMSource(doc),
+             new StreamResult(new OutputStreamWriter(out, "utf-8"));
+             You must do (3) to workaround a "buggy" behavior of the
+             xml handling code.
+             */
+            TransformerFactory tf = TransformerFactory.newInstance();
+            tf.setAttribute("indent-number", new Integer(2));
+            Transformer xformer = tf.newTransformer();
+            xformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            Source source = new DOMSource(document);
+            xformer.transform(source, result);
+
+            // don't add doctype to SVG files. see http://jwatt.org/svg/authoring/
+            /*
+             xformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, svgIdentifier);
+             xformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, svgDTD);
+             */
+            outputStreamWriter.flush();
+
+        } catch (Exception e) {
+            String msg = e.getMessage() != null ? e.getMessage() : e.getClass().toString();
+            throw new IOException("Export to SVG not possible. " + msg);
+        }
+    }
+
+    /**
+     * Creates the top level SVG element.
+     *
+     * @param document The document to export to.
+     * @return
+     */
+    protected Element createSVGRootElement(Document document) {
+        // create the main svg element
+        Element svg = (Element) document.createElementNS(SVGNAMESPACE, "svg");
+
+        // specify a namespace prefix on the 'svg' element, which means that
+        // SVG is the default namespace for all elements within the scope of
+        // the svg element with the xmlns attribute:
+        // See http://www.w3.org/TR/SVG11/struct.html#SVGElement
+        // and http://jwatt.org/svg/authoring/
+        svg.setAttribute("xmlns", SVGNAMESPACE);
+        svg.setAttribute("xmlns:xlink", XLINKNAMESPACE);
+        svg.setAttribute("xmlns:ev", XMLEVENTSNAMESPACE);
+        svg.setAttribute("version", "1.0");
+        svg.setAttribute("preserveAspectRatio", "xMinYMin");
+
+        /*
+         FIXME
+         final double wWC = pageFormat.getPageWidthWorldCoordinates();
+         final double hWC = pageFormat.getPageHeightWorldCoordinates();
+         final double w = dimToPageRoundedPx((float) wWC);
+         final double h = dimToPageRoundedPx((float) hWC);
+         svg.setAttribute("width", Double.toString(w));
+         svg.setAttribute("height", Double.toString(h));
+
+         // Define the viewBox.
+         String viewBoxStr = "0 0 " + w + " " + h;
+         svg.setAttribute("viewBox", viewBoxStr);*/
+        return svg;
+    }
+
+    /**
+     * Append author and application name to SVG document.
+     *
+     * @param svgRootElement Destination element.
+     * @param document SVG document.
+     */
+    protected void appendDescription(Element svgRootElement, Document document) {
+
+        StringBuilder str = new StringBuilder();
+        if (authorName != null && authorName.length() > 0) {
+            str.append("Author:");
+            str.append(authorName);
+            str.append(" - ");
+        }
+
+        if (applicationName != null && applicationName.length() > 0) {
+            str.append("Generator:");
+            str.append(applicationName);
+            str.append(" - ");
+        }
+        str.append("Date:");
+        str.append(java.util.Calendar.getInstance().getTime());
+
+        // create a description element
+        Element desc = (Element) document.createElementNS(SVGNAMESPACE, "desc");
+        desc.appendChild(document.createTextNode(str.toString()));
+
+        // append description element
+        svgRootElement.appendChild(desc);
+    }
+
+    /**
+     * Appends a GeometryCollection to a DOM element.
+     *
+     * @param collection The GeometryCollection to convert.
+     * @param parent The parent element that will contain the passed
+     * GeometryCollection.
+     * @param document The DOM.
+     */
+    private void appendGeometryCollection(GeometryCollection collection,
+            Element parent, Document document) throws IOException {
+
+        Element g = document.createElementNS(SVGNAMESPACE, "g");
+        if (g == null) {
+            return;
+        }
+        setVectorSymbol(g);
+        parent.appendChild(g);
+
+        final int nbrObj = collection.getNumGeometries();
+        for (int i = 0; i < nbrObj; i++) {
+            Geometry geometry = collection.getGeometryN(i);
+
+            if (geometry instanceof GeometryCollection) {
+                appendGeometryCollection((GeometryCollection) geometry, g, document);
+            } else {
+                if (geometry instanceof LineString) {
+                    appendLineString((LineString) geometry, g, document);
+                } /* else if (geometry instanceof GeoText) {
+                 writeGeoText((GeoText) geometry, parent, doc);
+                 }*/
+
+            }
+        }
+    }
+
+    /**
+     * Append a LineString geometry to an SVG document
+     *
+     * @param lineString The geometry.
+     * @param parent The destination element.
+     * @param document The SVG document.
+     */
+    private void appendLineString(LineString lineString, Element parent, Document document) {
+        String svgPath = convertLineStringToSVGString(lineString);
+        Element pathElement = (Element) document.createElementNS(SVGNAMESPACE, "path");
+        pathElement.setAttribute("d", svgPath);
+        parent.appendChild(pathElement);
+    }
+
+    /**
+     * Converts a LineString to a SVG string for the d attribute of a path
+     * element.
+     *
+     * @param lineString The LineString to convert.
+     * @return A string for the d attribute of a SVG path element.
+     */
+    private String convertLineStringToSVGString(LineString lineString) {
+        StringBuilder str = new StringBuilder();
+        Point startPoint = lineString.getStartPoint();
+        str.append("M");
+        str.append(df.format(xToPagePx(startPoint.getX())));
+        str.append(" ");
+        str.append(df.format(yToPagePx(startPoint.getY())));
+
+        int numPoints = lineString.getNumPoints();
+        for (int i = 1; i < numPoints; i++) {
+            Point point = lineString.getPointN(i);
+            str.append(" L");
+            str.append(df.format(xToPagePx(point.getX())));
+            str.append(" ");
+            str.append(df.format(yToPagePx(point.getY())));
+        }
+
+        return str.toString();
+    }
+
+    /**
+     * append vector style to element
+     * @param element 
+     */
+    private void setVectorSymbol(Element element) {
+        String strokeColor = ColorUtils.colorToCSSString(Color.BLACK);
+        element.setAttribute("stroke", strokeColor);
+        String fillColor = "none";
+        element.setAttribute("fill", fillColor);
+        double strokeWidth = 1;
+        element.setAttribute("stroke-width", Double.toString(strokeWidth));
+    }
+
+     /**
+     * Transforms a horizontal x coordinate (usually in meters) to pixels. Takes 
+     * the scale and bounding box defined by the PageFormat into account.
+     * @param x The horizontal coordinate.
+     * @return Returns the coordinate in pixels.
+     */
+    protected double xToPagePx(double x) {
+        double west = collectionBoundingBox.getMinX();
+        return (x - west) / mapScale * 1000 * MM2PX;
+    }
+    
+    /**
+     * Transforms a vertical y coordinate to the scale 
+     * and bounding box defined by the PageFormat.
+     * @param y The vertical coordinate.
+     * @return Returns the coordinate in the page coordinate system.
+     */
+    protected double yToPagePx(double y) {
+        double north = collectionBoundingBox.getMaxY();
+        return (north - y) / mapScale * 1000 * MM2PX;
+    }
+   
+}
