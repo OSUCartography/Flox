14c47c29fc4108767790ccda5419191fda4f5b34
stephdan <stephdan@onid.oregonstate.edu>
2015-04-24 16:40:54 -0700

    Began to develop BezierFlow constructor from points

--- src/edu/oregonstate/cartography/flox/model/BezierFlow.java
+++ src/edu/oregonstate/cartography/flox/model/BezierFlow.java
@@ -20,6 +20,49 @@ public class BezierFlow extends Flow {
         this.endPt = new Point(endX, endY);
     }
 
+    /**
+     * Construct a simple BesierFlow from 2 Point objects
+     *
+     * @param startPt
+     * @param endPt
+     */
+    public BezierFlow(Point startPt, Point endPt) {
+
+        double x1 = startPt.x;
+        double x2 = endPt.x;
+        double y1 = startPt.y;
+        double y2 = endPt.y;
+
+        // Angle between the straight line connecting start and end point and 
+        // the line connecting the start/end point with the corresponding Bezier 
+        // control point.
+        double alpha = 0.3;
+        
+        // Distance between startPt and endPt
+        double dist = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+        
+    }
+
+    /**
+     * Computes the azimuthal angle for a line between a start and end point
+     * @return Angle in radians
+     */
+    private double getBaselineAzimuth() {
+        final double dx = endPt.x- startPt.x;
+        final double dy = endPt.y - startPt.y;
+        return Math.atan2(dx, dy);
+    }
+
+    // Need the distance between the points
+    private void computeStartCtrlPt(double alpha, double dist) {
+        final double lineOrientation = getBaselineAzimuth();
+        final double azimuth1 = lineOrientation + alpha;
+        final double dx1 = Math.sin(azimuth1) * dist;
+        final double dy1 = Math.cos(azimuth1) * dist;
+        //this.cPt1 = this. + dx1;
+    }
+
     @Override
     public Rectangle2D.Double getBoundingBox() {
         Rectangle2D.Double bb = new Rectangle2D.Double(startPt.x, startPt.y, 0, 0);
@@ -28,7 +71,7 @@ public class BezierFlow extends Flow {
         bb.add(cPt2.x, cPt2.y);
         return bb;
     }
-    
+
     /**
      * @return the cPt1
      */
