3dae8e2f9fa4899bd52cfee3ec5d16db3480f218
stephdan <stephdan@onid.oregonstate.edu>
2015-05-03 22:27:50 -0700

    Added LayoutGrader class, began some pseudocode for a countFlowIntersections method.
    Began some pseudocode for a countFlowIntersections method.

---
+++ src/edu/oregonstate/cartography/flox/model/LayoutGrader.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ */
+package edu.oregonstate.cartography.flox.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dan Stephen
+ */
+public class LayoutGrader {
+    
+    public int countFlowIntersections (ArrayList<Flow> flows) {
+
+        int intersections = 0;
+        // get a flow iterator
+        
+        // convert each flow toStraightLineSegment(0.01)
+        
+        // Create a polyline gemetry from the flow
+        
+        // add it to a GeometryCollection
+        
+        // For each flow in the collection, iterate through all the other
+        // flows to see if it intersects it. If it does, add one to a counter
+        
+        // figure out a way to prevent it from counting intersections twice?
+        // Maybe by removing a flow from the collection after it's been checked?
+        
+        
+        // return the counter? Display it in the GUI? Make a button that 
+        // does this?
+        
+        return intersections;
+    }
+    
+}
