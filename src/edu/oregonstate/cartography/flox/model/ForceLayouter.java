b5ab7760dcb8d13d6e30516a6eb02d99d99ac9bb
stephdan <stephdan@onid.oregonstate.edu>
2015-05-05 23:22:35 -0700

    Added ForceLayouter class, added computeTotalForce method

---
+++ src/edu/oregonstate/cartography/flox/model/ForceLayouter.java
@@ -0,0 +1,31 @@
+package edu.oregonstate.cartography.flox.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author danielstephen
+ */
+public class ForceLayouter {
+
+    // Stores the model
+    Model model;
+
+    // Sets the model
+        this.model = model;
+    }
+
+    /**
+     * Computes the total acting force on a point as applied by neighboring
+     * points
+     *
+     * @return
+     */
+
+            
+            
+    }
+
+}
+
