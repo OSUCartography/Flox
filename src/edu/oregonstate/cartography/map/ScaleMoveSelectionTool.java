38edc553a693e5d791b19892018517e438c525cd
Daniel Stephen <stephdan@onid.oregonstate.edu>
2015-08-04 19:40:52 -0700

    Added lock/unlock button to the top toolbar

--- src/edu/oregonstate/cartography/map/ScaleMoveSelectionTool.java
+++ src/edu/oregonstate/cartography/map/ScaleMoveSelectionTool.java
@@ -8,6 +8,7 @@
 package edu.oregonstate.cartography.map;
 
 import edu.oregonstate.cartography.simplefeature.AbstractSimpleFeatureMapComponent;
+import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
 
 
@@ -22,11 +23,12 @@ public class ScaleMoveSelectionTool extends CombinedTool {
      * Creates a new instance of RotateScaleMoveSelectionTool
      */
     public ScaleMoveSelectionTool(AbstractSimpleFeatureMapComponent mapComponent, 
-            JFormattedTextField valueField, JFormattedTextField xField, JFormattedTextField yField) {
+            JFormattedTextField valueField, JFormattedTextField xField, 
+            JFormattedTextField yField, JButton lockUnlockButton) {
         super(mapComponent, "Select - Move - Scale");
         
         SelectionTool selectionTool = new SelectionTool(this.mapComponent, 
-                valueField, xField, yField);
+                valueField, xField, yField, lockUnlockButton);
         
         MoveTool moveTool = new MoveTool(this.mapComponent, xField, yField);
     
