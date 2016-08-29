e12ed006c9da097fd6413316b6cb90592e84ef31
Bernie Jenny <jennyb@geo.oregonstate.edu>
2016-08-29 17:23:23 +1000

    Removed obsolete copying of lock status when moving flows off nodes

--- src/edu/oregonstate/cartography/flox/gui/MainWindow.java
+++ src/edu/oregonstate/cartography/flox/gui/MainWindow.java
@@ -3457,14 +3457,7 @@ public class MainWindow extends javax.swing.JFrame {
                 layouter.layoutAllFlows(weight);
 
                 if (moveFlowsOverlappingNodes) {
-                    // store initial lock flags of all flows
-                    boolean[] initialLocks = model.getLocks();
-
-                    // move flows: this will lock flows that have been moved
                     layouter.moveFlowsOverlappingObstacles();
-
-                    // reset lock flags to initial values
-                    model.applyLocks(initialLocks);
                 }
 
                 // publish intermediate results in map. This will call process() 
